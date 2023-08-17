af72ac00226fd0c7f3c2f473d0e99bbc5dd627c7
Pham Duc Tho <20041501+thopd88@users.noreply.github.com>
2023-08-17 14:01:25 +0700

    API sample
    Book listing API

---
+++ BookResource/app/Http/Controllers/api/BookController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        if (!auth()->check()) {
+            return response()->json(['error'=>'YOU need to login first'], 401);;
+        }
+        $books = Book::all();
+        return response()->json($books, 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
